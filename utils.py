17fa004b85dbfd4a8c87ec4e26003b6c6ba41d64
L'Brain James V2 <143547261+jmwelshV2@users.noreply.github.com>
2023-12-12 16:56:27 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,5 @@
+class Point:
+    def __init__(self, density, compressive_strength, tensile_strength):
+        self.density = density
+        self.compressive_strength = compressive_strength
+        self.tensile_strength = tensile_strength
