cee824debb96106fb622327e40c95ab0185e233f
L'Brain James V2 <143547261+jmwelshV2@users.noreply.github.com>
2023-12-12 16:53:12 -0500

    Delete DefinePoint.py

--- DefinePoint.py
+++
@@ -1,5 +0,0 @@
-class Point:
-    def __init__(self, density, compressive_strength, tensile_strength):
-        self.density = density
-        self.compressive_strength = compressive_strength
-        self.tensile_strength = tensile_strength
