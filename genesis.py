44dc74b0d3c363873b5bd0408e6ceeee5bfcd815
L'Brain James V2 <143547261+jmwelshV2@users.noreply.github.com>
2023-12-12 16:57:23 -0500

    Update genesis.py

--- genesis.py
+++ genesis.py
@@ -1,1 +1,13 @@
+import matplotlib.pyplot as plt
 
+size = 10  # Starting size
+max_iterations = 100
+best_structure = generate_initial_structure(size)
+
+for iteration in range(max_iterations):
+    new_structure = evolve_structure(best_structure)
+    if evaluate_structure(new_structure):
+        best_structure = new_structure
+        # Save the structure as an image
+        plt.imshow(best_structure)  # This will need to be adapted to visualize the structure
+        plt.savefig(f"structure_{iteration}.jpg")
