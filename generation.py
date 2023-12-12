24f60f2eb229c32627fd15efc8a1fe342b8f8bcf
L'Brain James V2 <143547261+jmwelshV2@users.noreply.github.com>
2023-12-12 16:59:25 -0500

    Update generation.py

--- generation.py
+++ generation.py
@@ -1,1 +1,5 @@
+import numpy as np
 
+def generate_initial_structure(size):
+    # Placeholder values for density, compressive_strength, tensile_strength
+    return np.array([[Point(1, 100, 100) for _ in range(size)] for _ in range(size)])
