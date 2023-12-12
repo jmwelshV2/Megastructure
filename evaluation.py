876ffb9db623e10f488eda558318e2482f6dbe39
L'Brain James V2 <143547261+jmwelshV2@users.noreply.github.com>
2023-12-12 17:00:51 -0500

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,1 +1,4 @@
-
+def evaluate_structure(structure):
+    # Check if the structure can be scaled while maintaining the allowance factor
+    # This will likely involve running the physics simulation at different scales
+    pass
